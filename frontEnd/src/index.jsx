97d63166fbad280a37567b8163b97328ca006714
thauan <thauanandre09@gmail.com>
2022-01-11 11:35:20 -0300

    add  components React

---
+++ frontEnd/src/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import app from "./main/app";
+
+ReactDOM.render(<app></app>,document.getElementById('app'))
---
+++ frontEnd/src/main/app.jsx
@@ -0,0 +1,10 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+import Todo from '../todo/todo'
+import about from '../about/about'
+import React from 'react'
+export default props=>(
+    <div className="container">
+        <Todo></Todo>
+    </div>
+)
